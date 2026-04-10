67a95488fa789f7052d894ab76894f95ff59e36b
Jayden Andrews <jaydenrandrews@gmail.com>
2026-04-10 11:23:09 -0400

    Added music functionality

--- fetch-quest-lab/src/App.jsx
+++ fetch-quest-lab/src/App.jsx
@@ -1,20 +1,33 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
 import TypeSelector from './components/TypeSelector'
 import PokemonGrid from './components/PokemonGrid'
 
 function App() {
   const [type, setType] = useState('')
+  const audioRef = useRef(null)
+  const [musicStarted, setMusicStarted] = useState(false)
+
+  const toggleMusic = () => {
+    if (!musicStarted) {
+      audioRef.current.play()
+      setMusicStarted(true)
+    }
+  }
 
   return (
-  <div className="app">
-    <div className="pokedex-screen">
-      <h1 className="app-title">Pokemon Viewer</h1>
-      <TypeSelector setType={setType} />
-      <PokemonGrid type={type} />
+    <div className="app">
+      <audio ref={audioRef} src="./src/assets/20 - Pokémon Center.mp3" loop />
+      <div className="pokedex-screen">
+        <div className="title-bar">
+          <h1 className="app-title">Pokédex</h1>
+          <button className="music-btn" onClick={toggleMusic}></button>
+        </div>
+        <TypeSelector setType={setType} />
+        <PokemonGrid type={type} />
+      </div>
     </div>
-  </div>
-)
+  )
 }
 
 export default App
